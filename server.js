123451c5a74e49130a763cf5b8a3ef99941bf013
keurid <kevindesantiago99@gmail.com>
2023-08-14 18:37:59 -0700

    refactored code and added comments

--- server.js
+++ server.js
@@ -2,19 +2,29 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const sequelize = require('./config/connection');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
-const hbs = exphbs.create({});
 
-app.engine('handlebars', hbs.engine);
+// Middleware
+app.use(express.json()); // JSON parsing
+app.use(express.urlencoded({ extended: false })); // URL-encoded parsing
+app.use(express.static(path.join(__dirname, 'public'))); // Static files
+
+// Handlebars configuration
+app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
+// Routes
 app.use(require('./controllers'));
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
+// Start the server after syncing the database
+sequelize.sync({ force: false })
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server listening on port ${PORT}`);
+        });
+    })
+    .catch(err => {
+        console.error('Database connection error:', err);
+    });
