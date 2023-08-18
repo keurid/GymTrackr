4572a981dc9768b87a0e126232fa753117095d80
keurid <kevindesantiago99@gmail.com>
2023-08-17 18:57:37 -0700

    fixed everything, thank u chris

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -24,15 +24,17 @@ const loginFormHandler = async (event) => {
 
 const signupFormHandler = async (event) => {
   event.preventDefault();
-
-  const name = document.querySelector('#name-signup').value.trim();
+  console.log("hello")
+  const username = document.querySelector('#username-signup').value.trim();
   const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
-
-  if (name && email && password) {
+console.log(username)
+console.log(email)
+console.log(password)
+  if (username && email && password) {
     const response = await fetch('/api/users', {
       method: 'POST',
-      body: JSON.stringify({ name, email, password }),
+      body: JSON.stringify({ username, email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
 
